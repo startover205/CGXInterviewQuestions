7d2714e59a4f4c9bd15c8c688785e382253c2bcf
Ming-Ta Yang <startover205@gmail.com>
2023-12-17 23:45:00 +0800

    Initial Commit

---
+++ CGXInterviewQuestions/CGXInterviewQuestionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CGXInterviewQuestionsApp.swift
+//  CGXInterviewQuestions
+//
+//  Created by Ming-Ta Yang on 2023/12/17.
+//
+
+import SwiftUI
+
+@main
+struct CGXInterviewQuestionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CGXInterviewQuestions/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CGXInterviewQuestions
+//
+//  Created by Ming-Ta Yang on 2023/12/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
